57240f328ba60c41fffc355c71c1ed93669ea6e9
Tim Killian <tskillian@gmail.com>
2013-12-10 18:34:49 -0800

    added super basic chat functionality that looks terrible

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ var path = require("path"),
     io = require("socket.io"),
     http = require("http"),
     app = express();
-
+    
+app.use(express.bodyParser());
 
 // The server holds the state of the 'classroom' in this global object. 
 var Classroom = {};
+
+//Initialize an empty array that will contain objects of each chat participant
+var participants = [];
 
 // functions that modify the Classroom object
 function addClient(clientId) {
@@ -46,6 +50,28 @@ app.get("/client", function(req, res) {
     //res.render("client");
 });
 
+//POST method to create a chat message
+app.post("/message", function(request, response) {
+    console.log(request);
+  //The request body expects a param named "message"
+  var message = request.body.message;
+
+  //If the message is empty or wasn't sent it's a bad request
+  if(_.isUndefined(message) || _.isEmpty(message.trim())) {
+    return response.json(400, {error: "Message is invalid"});
+  }
+
+  //We also expect the sender's name with the message
+  var name = request.body.name;
+
+  //Let our chatroom know there was a new message
+  io.sockets.emit("incomingMessage", {message: message, name: name});
+
+  //Looks good, let the client know
+  response.json(200, {message: "Message received"});
+
+});
+
 // create and start io server thing
 var server = http.createServer(app);
     io = io.listen(server);
@@ -68,6 +94,22 @@ var server = http.createServer(app);
         client.on('disconnect', function(){
             removeClient(client.id);
             io.sockets.emit("update", Classroom);
+        });
+
+
+        client.on("newUser", function(data) {
+            participants.push({id: data.id, name: data.name});
+            io.sockets.emit("newConnection", {participants: participants});
+        });
+
+        client.on("nameChange", function(data) {
+            _.findWhere(participants, {id: client.id}).name = data.name;
+            io.sockets.emit("nameChanged", {id: data.id, name: data.name});
+        });
+
+        client.on("disconnect", function() {
+            participants = _.without(participants,_.findWhere(participants, {id: client.id}));
+            io.sockets.emit("userDisconnected", {id: client.id, sender:"system"});
         });
     });
 
