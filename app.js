ff3cddd4267546f3d4529c2472f7968639c3e6dd
Jesse Rathbun <walkingfishstudios@gmail.com>
2013-12-12 14:14:50 -0800

    added Ryan's app and main files to base project

--- app.js
+++ app.js
@@ -6,22 +6,52 @@ var path = require("path"),
     app = express();
 
 
+
+
+
+
 // The server holds the state of the classrooms in this global object. 
+// "schema":
+// Classrooms: {
+//     classroomId: {
+//         clientId: ##id from websocket##
+//     }
+// }
 var Classrooms = {};
 
-// functions that modify the Classrooms object
-function addTeacher(classroomId, clientId) {
 
-    Classrooms[classroomId][clientId] = {status: "none", comment: ""};
-    // 
+
+//  the server holds student names associated with websockets in this object
+//  "schema":
+//  nameRegister: {
+//      classroomId: {
+//          ##id from websocket##: ##student name##
+//      }
+//  }
+var nameRegister = {};
+
+// functions that modify the Classrooms and nameRegister objects
+
+// function addTeacher(classroomId, clientId) {
+// !!!! might not need this. yet?
+// }
+function createClassroom(classroomId) {
+    Classrooms[classroomId] = {};
+    nameRegister[classroomId] = {};
 }
-function addStudent(classroomId, clientId) {
-    if (!Classrooms[classroomId]) {
-        Classrooms[classroomId] = {};
-    }
+function removeClassroom(classroomId) {
+    // disconnect all sockets?
+    delete Classrooms[classroomId];
+    delete nameRegister[classroomId];
+}
+function addStudent(classroomId, clientId, studentName) {
+    // work around until teacher creating classrooms is working
+    // replace with error message about no class to join?
+    // if (!Classrooms[classroomId]) {
+    //     createClassroom(classroomId)
+    // }
     Classrooms[classroomId][clientId] = {status: "none", comment: ""};
-    console.log(Classrooms);
-    // [classroomId]
+    nameRegister[classroomId][clientId] = studentName;
 }
 function setStatus(classroomId, clientId, updatedStatus) {
     Classrooms[classroomId][clientId].status = updatedStatus;
@@ -33,7 +63,7 @@ function setComment(classroomId, clientId, updatedComment) {
 }
 function removeClient(classroomId, clientId) {
     delete Classrooms[classroomId][clientId];
-    // [classroomId]
+    delete nameRegister[classroomId][clientId];
 }
 
 // ExpressJS Server Definition
@@ -43,49 +73,79 @@ app.set("views", path.join(__dirname, ""))
    .use(express.static(path.join(__dirname, "js")));
 
 app.get("/", function(req, res) {
+    console.log("newindex hit");
+    res.render("newIndex");
+});
+
+app.get("/oldIndex", function(req, res) {
     console.log("index hit");
     res.render("index");
 });
 
-app.get("/server", function(req, res) {
-    console.log("server hit");
-    //res.render("server");
+app.get("/student", function(req, res) {
+    console.log("student route hit");
+    res.render("student");
 });
 
-app.get("/client", function(req, res) {
-    console.log("server hit");
-    //res.render("client");
+app.get("/teacher", function(req, res) {
+    console.log("teacher route hit");
+    res.render("teacher");
 });
 
-// create and start io server thing
+// create app server, wrap it in websocket server
 var server = http.createServer(app);
     io = io.listen(server);
 
-    io.sockets.on('connection', function(client) {
+// websocket behavior
+io.sockets.on('connection', function(client) {
+    //after join, emit list of available classrooms
+   client.emit('classroomsUpdate', _.keys(Classrooms));
 
-
-        client.on('joinClassroom', function(classroomId){
-            client.join(classroomId);
-            addStudent(classroomId, client.id)
-            io.sockets.in(classroomId).emit("update", Classrooms[classroomId]);
-        
-            client.on('setStatus', function(status) {
-                setStatus(classroomId, client.id, status);
-                io.sockets.in(classroomId).emit("update", Classrooms[classroomId]);
-            });
-
-            client.on('setComment', function(comment) {
-                setComment(classroomId, client.id, comment);
-                io.sockets.in(classroomId).emit("update", Classrooms[classroomId]);
-            });
-
-            client.on('disconnect', function(){
-                removeClient(classroomId, client.id);
-                io.sockets.in(classroomId).emit("update", Classrooms[classroomId]);
-            });
-        });
-                
+    //create classroom currently before joining as teacher, potential security hole
+    client.on('createClassroom', function(data){
+        createClassroom(data);
+        console.log(Classrooms);
+        io.sockets.emit('classroomsUpdate', _.keys(Classrooms));
     });
+
+    //teacher connection
+    client.on('teacherJoinClassroom', function(classroomId){
+        client.join(classroomId);
+        client.emit('update', Classrooms[classroomId]);
+        client.emit('nameUpdate', nameRegister[classroomId]);
+        console.log("teacher joined classroom");
+
+
+        io.sockets.on('studentJoinClassroom', function(){
+
+        })
+
+    });
+
+    // student connection
+    client.on('studentJoinClassroom', function(classroomId, studentName){
+        client.join(classroomId);
+        addStudent(classroomId, client.id, studentName);
+        io.sockets.in(classroomId).emit("update", Classrooms[classroomId]);
+                console.log(Classrooms);
+        console.log(nameRegister);
+        client.on('setStatus', function(status) {
+            setStatus(classroomId, client.id, status);
+            io.sockets.in(classroomId).emit("update", Classrooms[classroomId]);
+        });
+
+        client.on('setComment', function(comment) {
+            setComment(classroomId, client.id, comment);
+            io.sockets.in(classroomId).emit("update", Classrooms[classroomId]);
+        });
+
+        client.on('disconnect', function(){
+            removeClient(classroomId, client.id);
+            io.sockets.in(classroomId).emit("update", Classrooms[classroomId]);
+        });
+    });
+            
+});
 
 // start web server
 var port = process.env.PORT || 3000;
--- js/main.js
+++ js/main.js
@@ -1,42 +1,92 @@
 // global app namespace
 window.App = {}
 
-$.get('client.html', function(res) {
-	App.clientTemplate = res;
+// get templates
+$.get('student.html', function(res) {
+	App.studentTemplate = res;
+});
+$.get('classroom.html', function(res) {
+	App.classroomTemplate = res;
 });
 
-//Backbone Client model
-App.Client = Backbone.Model.extend({
+//Backbone Classroom model
+App.Classroom = Backbone.Model.extend({
 	//override AJAX save method, don't need it/errors
 	save: function() { return null; }
 });
 
-// Backbone Classroom collection
-App.Classroom = Backbone.Collection.extend({
-	model: App.Client
+//Backbone Classrooms Collection
+App.Classrooms = Backbone.Collection.extend({
+	model: App.Classroom
 });
 
-// Classroom View
-App.ClassroomView = Backbone.View.extend({
+// Classrooms Collection View
+App.ClassroomsView = Backbone.View.extend({
 	el: "#dropzone",
 	initialize: function() {
 		this.render();
 	},
 	render: function() {
 		this.collection.each(function(model){
-            App.clientView = new App.ClientView({model:model});
-            this.$el.append(App.clientView.el);
+            App.classroomView = new App.ClassroomView({model:model});
+            this.$el.append(App.classroomView.el);
     	}, this);
 	}
 });
-// Client View
-App.ClientView = Backbone.View.extend({
+// Classroom Model View
+App.ClassroomView = Backbone.View.extend({
+	tagName: null, // <--- super hacky way to stop backbone wrapping this in an extra div
+	initialize: function() {
+		this.render();
+	},
+	// events: {
+	// 	'click': 'joinClassroom'
+	// },
+	// joinClassroom: function(){
+	// 	socket.emit('studentJoinClassroom', this.model.get('name'), studentName);
+	// },
+	render: function() {
+		var template = Handlebars.compile(App.classroomTemplate);
+		this.$el.html(template(this.model.toJSON()));
+	}
+});
+
+
+
+
+
+//Backbone Student model
+App.Student = Backbone.Model.extend({
+	//override AJAX save method, don't need it/errors
+	save: function() { return null; }
+});
+
+// Backbone Students collection
+App.Students = Backbone.Collection.extend({
+	model: App.Student
+});
+
+// Students Collection View
+App.StudentsView = Backbone.View.extend({
+	el: "#dropzone",
+	initialize: function() {
+		this.render();
+	},
+	render: function() {
+		this.collection.each(function(model){
+            App.studentView = new App.StudentView({model:model});
+            this.$el.append(App.studentView.el);
+    	}, this);
+	}
+});
+// Student Model View
+App.StudentView = Backbone.View.extend({
 	tagName: null, // <--- super hacky way to stop backbone wrapping this in an extra div
 	initialize: function() {
 		this.render();
 	},
 	render: function() {
-		var template = Handlebars.compile(App.clientTemplate);
+		var template = Handlebars.compile(App.studentTemplate);
 		this.$el.html(template(this.model.toJSON()));
 	}
 });
