6040ded1bb06c220493e4b52384fefa02467a4c4
Patrick <costanzo.patrick@gmail.com>
2013-12-12 17:01:12 -0800

    refactored view rendering, views, view events, and templates on teacher page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // global app namespace
-window.App = {}
+var App = window.App || {};
 
 // get templates
 $.get('statusBlock.html', function(res) {
@@ -20,6 +20,55 @@ App.Classroom = Backbone.Model.extend({
 //Backbone Classrooms Collection
 App.Classrooms = Backbone.Collection.extend({
 	model: App.Classroom
+});
+
+App.TeacherCreateClassroomParentView = Backbone.View.extend({
+	el: "#contentArea",
+
+	events: {
+		"click #createClassroom" : "createClassroom",
+		"submit #createClassroomForm" : "createClassroom",
+		"click .classroom": "joinClassroom"
+	},
+
+	initialize: function() {
+		this.render();
+		var collection = this.collection;
+	},
+
+	joinClassroom: function() {
+		socket.emit('teacherJoinClassroom', $(this).attr("data"));
+	},
+
+	createClassroom: function(e) {
+      e.preventDefault();
+
+      //--------
+      // This section trims whitespace before or after the
+      // classroom name, prevents submissions from being blank
+      // and then clears the input field after submission.
+      //--------
+
+      var classroomName = $("[name='classroomName']");
+      var trimmedName = classroomName.val().replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+      if (trimmedName.length === 0) {
+        return;
+      }
+      socket.emit('createClassroom', trimmedName);
+      classroomName.val("");
+	},
+
+	render: function(){
+		var source = $("#teacherCreateClassroomParent").html();
+		var template = Handlebars.compile(source);
+		var html = template();
+		this.$el.html(html);
+
+        App.classroomsView = new App.ClassroomsView({
+          collection: App.classrooms
+        });
+	}
+
 });
 
 // Classrooms Collection View
